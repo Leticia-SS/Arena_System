495720a2fdad585d13377a53fb78f247462179ad
Leticia-SS <leticia.saraiva013@gmail.com>
2026-05-12 22:45:33 -0300

    initial commit

---
+++ src/main/java/com/example/arenasystem/ArenaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.arenasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/arenasystem/ArenaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arenasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
