8d3414f80cdd0381f9fd2634af67178203d34d1e
Leticia-SS <leticia.saraiva013@gmail.com>
2026-05-31 15:34:11 -0300

    implement eureka server

---
+++ arena-service/src/test/java/com/example/arena_service/ArenaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arena_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
