4efbdf877df93549d4a31f19bfe403a3942c7413
Leticia-SS <leticia.saraiva013@gmail.com>
2026-06-02 20:40:06 -0300

    update character service

--- arena-service/src/main/java/com/example/arena_service/repository/ICharacterRepository.java
+++ arena-service/src/main/java/com/example/arena_service/repository/ICharacterRepository.java
@@ -3,6 +3,8 @@ package com.example.arena_service.repository;
 import com.example.arena_service.model.Character;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ICharacterRepository extends MongoRepository<Character, String> {
+import java.util.Optional;
 
+public interface ICharacterRepository extends MongoRepository<Character, String> {
+    Optional<Character> findByName(String name);
 }
