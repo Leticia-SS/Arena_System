7ae0dcfa15a787d5f370dd7b6c560a90c7314082
Leticia-SS <leticia.saraiva013@gmail.com>
2026-06-02 00:28:44 -0300

    ajust Character repository import problem

--- arena-service/src/main/java/com/example/arena_service/repository/ICharacterRepository.java
+++ arena-service/src/main/java/com/example/arena_service/repository/ICharacterRepository.java
@@ -1,5 +1,6 @@
 package com.example.arena_service.repository;
 
+import com.example.arena_service.model.Character;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ICharacterRepository extends MongoRepository<Character, String> {
