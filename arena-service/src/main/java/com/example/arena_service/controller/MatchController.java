2e65dd6571f106049bc40fa8e9d974a3b7d14610
Leticia-SS <leticia.saraiva013@gmail.com>
2026-06-05 19:00:39 -0300

    match controller

---
+++ arena-service/src/main/java/com/example/arena_service/controller/MatchController.java
@@ -0,0 +1,42 @@
+package com.example.arena_service.controller;
+
+import com.example.arena_service.dto.MatchRequestDto;
+import com.example.arena_service.dto.MatchResponseDto;
+import com.example.arena_service.dto.TurnRequestDto;
+import com.example.arena_service.dto.TurnResponseDto;
+import com.example.arena_service.service.MatchService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/matches")
+public class MatchController {
+    private final MatchService matchService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MatchResponseDto> getMatchById(@PathVariable String id){
+        return ResponseEntity.ok(matchService.findById(id));
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<MatchResponseDto>> getMatchByUserId(@PathVariable String userId){
+        return ResponseEntity.ok(matchService.findByUserId(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<MatchResponseDto> createMatch(@RequestBody MatchRequestDto match){
+        return ResponseEntity.status(HttpStatus.CREATED).body(matchService.create(match));
+    }
+
+    @PostMapping("/{id}/turn")
+    public ResponseEntity<TurnResponseDto> executeTurn(@PathVariable String id, @RequestBody TurnRequestDto turn){
+        return ResponseEntity.ok(matchService.playTurn(id, turn));
+    }
+
+
+}
