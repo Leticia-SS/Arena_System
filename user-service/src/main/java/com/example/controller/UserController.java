dc4a7631dfbc61700e5d7a3a8881571036f3f51d
SarahInfnet <sasafipe@gmail.com>
2026-06-08 13:42:47 -0300

    Adiciona novas rotas aos users

--- user-service/src/main/java/com/example/controller/UserController.java
+++ user-service/src/main/java/com/example/controller/UserController.java
@@ -1,17 +1,18 @@
 package com.example.controller;
 
 import com.example.dto.ScoreDto;
+import com.example.dto.UpdateScoreRequest;
 import com.example.dto.UserDto;
+import com.example.dto.UserRankingDto;
 import com.example.model.Score;
 import com.example.model.User;
 import com.example.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -23,7 +24,6 @@ public class UserController {
     public UserController (UserService userService) {
         this.userService = userService;
     }
-
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable Long id){
@@ -44,5 +44,37 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.OK).body(userDto);
     }
+
+    @GetMapping("{id}/score")
+    public ResponseEntity<ScoreDto> getScore(@PathVariable Long id) {
+        ScoreDto scoreDto = userService.getScoreByUserId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(scoreDto);
+    }
+
+    @GetMapping("/ranking")
+    public ResponseEntity<List<UserRankingDto>> getRanking() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getRanking());
+    }
+
+    @GetMapping("/ranking/compare")
+    public ResponseEntity<List<UserRankingDto>> compare(
+            @RequestParam Long user1,
+            @RequestParam Long user2
+    ) {
+        return ResponseEntity.ok(
+                userService.compareUsers(user1, user2)
+        );
+    }
+
+    @PostMapping("{id}/score")
+    public ResponseEntity<Void> updateScore(
+            @PathVariable Long id,
+            @RequestBody UpdateScoreRequest request
+            ) {
+        userService.updateScore(id, request.victory());
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
--- user-service/src/main/java/com/example/repository/UserRepository.java
+++ user-service/src/main/java/com/example/repository/UserRepository.java
@@ -1,7 +1,22 @@
 package com.example.repository;
 
+import com.example.dto.UserRankingDto;
 import com.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("""
+        select new com.example.dto.UserRankingDto(
+            u.name,
+            s.points
+        )
+        from User u
+        join u.score s
+        order by s.points desc
+    """)
+    List<UserRankingDto> findRanking();
+
 }
